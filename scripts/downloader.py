0a919c43ea7cf12fb9ed5816e206bdfb8c20506a
nicholas <nicholasjcole1@gmail.com>
2022-12-01 21:38:21 -0800

    first

---
+++ scripts/downloader.py
@@ -0,0 +1,35 @@
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def main(url):
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, 'html.parser')
+	links = soup.find_all('a')
+	i = 0
+	files_saved = []
+	for link in links:
+		if ('.pdf' in link.get('href', [])):
+			i+=1
+			print("Downloading file: ", link.get('href', []))
+			# if else to add 'https' to links that don't have it
+			# requests don't pull if no https 
+			if link.get('href', [])[0:4] == 'http':
+				response = requests.get(link.get('href'))
+			else:
+				response = requests.get(('https:' + link.get('href')))
+			pdf = open("pdf"+str(i)+".pdf", 'wb')
+			pdf.write(response.content)
+			pdf.close()
+			print("File ", i, " downloaded")		
+			files_saved.append(link.get('href', []))
+	return files_saved
+
+
+if __name__ == '__main__':
+	# below url is placeholder
+    url = "https://investors.jfrog.com/events-and-presentations/presentations/default.aspx"
+    main(url)
+ 
---
+++ scripts/make_images.py
@@ -0,0 +1,15 @@
+import pypdfium2 as pdfium
+
+def make_images(filepath):
+    pdf = pdfium.PdfDocument(filepath)
+    page_indices = [i for i in range(len(pdf))]
+    renderer = pdf.render_to(
+        pdfium.BitmapConv.pil_image,
+	    page_indices = page_indices,
+    )
+    for image, index in zip(renderer, page_indices):
+        image.save("output_%02d.jpg" % index)
+
+if __name__ == '__main__':
+	filepath = "pdfs/pdf1.pdf"
+	make_images(filepath)
